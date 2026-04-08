ef7c35f9ce9d815cec44ea702f8a18caa38b39d0
anasMuf <anas.muhammadakbar@gmail.com>
2026-04-08 14:42:28 +0700

    fix: setup netlify vite plugin and remove conflicting netlify.toml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
 	plugins: [
@@ -12,6 +13,7 @@ const config = defineConfig({
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
+		netlify() as any,
 	],
 });
 
